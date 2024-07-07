855eed1e7a53552c8a5eff9c57175bcd5513eca6
rijankarki <rijkar58@gmail.com>
2024-07-07 22:03:42 +0545

    day1

---
+++ app.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const app=express();
+
+app.set('view engine','ejs')
+
+app.get("/",(req,res)=>{
+  res.send("<h1>This is Homepage</h1>")
+})
+
+app.get("/about",(req,res)=>{
+  const name="name"
+  res.render("about.ejs",{name:name})
+})
+app.get("/contact",(req,res)=>{
+
+  res.render("contact.ejs")
+})
+
+app.listen(3000,()=>{
+  console.log("Node Js has started at port:" + 3000);
+})
